d6a49ddc6a0e3a71096924882c2ee73548e6ed5c
hamed-zeidabadi <zeidabadi73@gmail.com>
2020-10-02 15:27:19 +0330

    chenge html file to php files

--- cms/js/default-assets/session-timeout-init.js
+++
@@ -1,1 +0,0 @@
-var SessionTimeout={init:function(){$.sessionTimeout({title:"لورم ایپسوم متن ساختگی  ",message:"لورم ایپسوم متن ساختگی با تولید سادگی ",redirUrl:"lock-screen.html",logoutUrl:"login.html",warnAfter:5e3,redirAfter:2e4,ignoreUserActivity:!0,countdownMessage:"زمان باقیمانده {timer} ثانیه.",countdownBar:!0})}};jQuery(document).ready(function(){SessionTimeout.init()});
