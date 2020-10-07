ff0aa2e8faf235961b205ccd1ec58a2f7bddcced
hamed-zeidabadi <zeidabadi73@gmail.com>
2020-10-07 08:41:29 +0330

    add componnet searchBar

--- index.php
+++ index.php
@@ -73,18 +73,10 @@
         <!-- Sidebar Widgets Column -->
         <div class="col-md-4">
 
-          <!-- Search Widget -->
-          <div class="card my-4">
-            <h5 style="font-size: 1.1em;" class="card-header">جستجو</h5>
-            <div class="card-body">
-              <div class="input-group">
-                <input style="font-size: 0.8em;" type="text" class="form-control" placeholder=" یه چی بنویس :)">
-                <span class="input-group-btn">
-                  <button style="font-size: 0.8em;margin-right: 4px;" class="btn btn-secondary" type="button">بزن بریم !</button>
-                </span>
-              </div>
-            </div>
-          </div>
+          <!-- start Search Widget -->
+          <?php include './search_bar.php' ?>
+          <!--  end  Search Widget -->
+
 
           <!-- Categories Widget -->
           <div class="card my-4">
