96932ab9bd39f971abaa47da1492cf968840d8e3
hamed-zeidabadi <zeidabadi73@gmail.com>
2020-10-07 09:47:49 +0330

    Add search functionality using tags

--- search.php
+++ search.php
@@ -38,8 +38,69 @@
 
 
         <!-- Start Post Content -->
+        <?php
 
-        <?php include 'post.php' ?>
+        $search_text = $_POST['search_text'];
+
+        if (!empty($search_text)) {
+          require_once 'includes/db.php';
+          $sql_get_posts = "SELECT * FROM posts_tbl WHERE post_content LIKE '%$search_text%'";
+          $query = mysqli_query($connect, $sql_get_posts);
+          while ($row = mysqli_fetch_assoc($query)) {
+
+            $title = $row['post_title'];
+            $author = $row['post_author'];
+            $date = $row['post_date'];
+            $image = $row['post_image'];
+            $content = $row['post_content'];
+            $tags = $row['post_tags'];
+            // gregorian_to_jalali
+            list($year, $month, $day) = explode('-', $date);
+            include_once 'includes/jdf.php';
+            $persian_date = gregorian_to_jalali($year, $month, $day, '/');
+
+        ?>
+
+            <?php ?>
+
+            <!-- Title -->
+            <h1 style="font-size: 1.8em;" class="mt-4"><?php echo $title ?></h1>
+
+            <!-- Author -->
+            <p style="font-size: 1em;" class="lead">
+              توسط
+              <a href="#"><?php echo $author ?></a>
+            </p>
+
+            <hr>
+
+            <!-- Date/Time -->
+            <p> آخرین بروزرسانی مقاله : <?php echo $persian_date ?></p>
+
+            <hr>
+
+            <!-- Preview Image -->
+            <img class="img-fluid rounded" src="<?php echo $image ?>" alt="">
+
+            <hr>
+
+            <!-- Post Content -->
+            <p style="font-size: 1.2em;" class="lead"><?php echo $content ?></p>
+
+            <hr>
+
+        <?php
+
+          };
+        }else{
+          echo '<h3 class ="m-3 h3 text-danger" >متاسفم ! چیزی پیدا نشد :( </h3>' ;
+        };
+        
+
+
+        ?>
+
+
 
         <!--  End Post Content -->
 
@@ -142,6 +203,6 @@
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 </body>
- 
- 
+
+
 </html>
--- search_bar.php
+++ search_bar.php
@@ -1,11 +1,13 @@
-  <div class="card my-4">
-      <h5 style="font-size: 1.1em;" class="card-header">جستجو</h5>
-      <div class="card-body">
-          <div class="input-group">
-              <input style="font-size: 0.8em;" type="text" class="form-control" placeholder=" یه چی بنویس :)">
-              <span class="input-group-btn">
-                  <button style="font-size: 0.8em;margin-right: 4px;" class="btn btn-secondary" type="button">بزن بریم !</button>
-              </span>
+  <form action="search.php" method="post">
+      <div class="card my-4">
+          <h5 style="font-size: 1.1em;" class="card-header">جستجو</h5>
+          <div class="card-body">
+              <div class="input-group">
+                  <input name="search_text" style="font-size: 0.8em;" type="text" class="form-control" placeholder=" یه چی بنویس :)">
+                  <span class="input-group-btn">
+                      <button style="font-size: 0.8em;margin-right: 4px;" name="search_btn" class="btn btn-secondary" type="submit">بزن بریم !</button>
+                  </span>
+              </div>
           </div>
       </div>
-  </div>
+  </form>
