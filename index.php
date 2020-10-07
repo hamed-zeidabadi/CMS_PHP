028652a2cdd769a1fe66dd3ab47fb2ea9b0d62f8
hamed-zeidabadi <zeidabadi73@gmail.com>
2020-10-07 08:37:59 +0330

    fix style in searchbar

--- index.php
+++ index.php
@@ -75,12 +75,12 @@
 
           <!-- Search Widget -->
           <div class="card my-4">
-            <h5 class="card-header">Search</h5>
+            <h5 style="font-size: 1.1em;" class="card-header">جستجو</h5>
             <div class="card-body">
               <div class="input-group">
-                <input type="text" class="form-control" placeholder="Search for...">
+                <input style="font-size: 0.8em;" type="text" class="form-control" placeholder=" یه چی بنویس :)">
                 <span class="input-group-btn">
-                  <button class="btn btn-secondary" type="button">Go!</button>
+                  <button style="font-size: 0.8em;" class="btn btn-secondary" type="button">بزن بریم !</button>
                 </span>
               </div>
             </div>
