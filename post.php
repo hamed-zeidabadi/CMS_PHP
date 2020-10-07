5eec5a00c52cf3cbe41be6bfecbeefe6da2b8aed
hamed-zeidabadi <zeidabadi73@gmail.com>
2020-10-07 08:32:07 +0330

    fix bug jalali date and add fake content iin project

--- post.php
+++ post.php
@@ -1,55 +1,55 @@
   <?php
-    require_once 'includes/db.php';
+  require_once 'includes/db.php';
 
-    $sql_get_posts = 'SELECT * FROM posts_tbl ';
-    $query = mysqli_query($connect, $sql_get_posts);
-    while ($row = mysqli_fetch_assoc($query)) {
+  $sql_get_posts = 'SELECT * FROM posts_tbl ';
+  $query = mysqli_query($connect, $sql_get_posts);
+  while ($row = mysqli_fetch_assoc($query)) {
 
-        $title = $row['post_title'];
-        $author = $row['post_author'];
-        $date = $row['post_date'];
-        $image = $row['post_image'];
-        $content = $row['post_content'];
-        $tags = $row['post_tags'];
+    $title = $row['post_title'];
+    $author = $row['post_author'];
+    $date = $row['post_date'];
+    $image = $row['post_image'];
+    $content = $row['post_content'];
+    $tags = $row['post_tags'];
 
 
-        // gregorian_to_jalali
-        list($year, $month, $day) = explode('-', $date);
-        include 'includes/jdf.php';
-        $persian_date = gregorian_to_jalali($year, $month, $day, '/');
+    // gregorian_to_jalali
+    list($year, $month, $day) = explode('-', $date);
+    include_once 'includes/jdf.php';
+    $persian_date = gregorian_to_jalali($year, $month, $day, '/');
 
-    ?>
+  ?>
 
-      <?php ?>
+    <?php ?>
 
-      <!-- Title -->
-      <h1 style="font-size: 1.8em;" class="mt-4"><?php echo $title ?></h1>
+    <!-- Title -->
+    <h1 style="font-size: 1.8em;" class="mt-4"><?php echo $title ?></h1>
 
-      <!-- Author -->
-      <p style="font-size: 1em;" class="lead">
-          توسط
-          <a href="#"><?php echo $author ?></a>
-      </p>
+    <!-- Author -->
+    <p style="font-size: 1em;" class="lead">
+      توسط
+      <a href="#"><?php echo $author ?></a>
+    </p>
 
-      <hr>
+    <hr>
 
-      <!-- Date/Time -->
-      <p> آخرین بروزرسانی مقاله : <?php echo $persian_date ?></p>
+    <!-- Date/Time -->
+    <p> آخرین بروزرسانی مقاله : <?php echo $persian_date ?></p>
 
-      <hr>
+    <hr>
 
-      <!-- Preview Image -->
-      <img class="img-fluid rounded" src="<?php echo $image ?>" alt="">
+    <!-- Preview Image -->
+    <img class="img-fluid rounded" src="<?php echo $image ?>" alt="">
 
-      <hr>
+    <hr>
 
-      <!-- Post Content -->
-      <p style="font-size: 1.2em;" class="lead"><?php echo $content ?></p>
+    <!-- Post Content -->
+    <p style="font-size: 1.2em;" class="lead"><?php echo $content ?></p>
 
-      <hr>
+    <hr>
 
   <?php
 
-    };
+  };
 
-    ?>
+  ?>
