6f7a402c7a958f1a07e39fa1d96f5ba04dbae38c
hamed-zeidabadi <zeidabadi73@gmail.com>
2020-10-09 15:03:24 +0330

    add sidebar componnet 'sidebar.php

--- index.php
+++ index.php
@@ -70,72 +70,31 @@
         <!-- Comment with nested comments -->
         <?php include 'comment.php' ?>
 
-        <!-- Sidebar Widgets Column -->
-        <div class="col-md-4">
+        <?php include 'sidebar.php' ?>
 
-          <!-- start Search Widget -->
-          <?php include './search_bar.php' ?>
-          <!--  end  Search Widget -->
-
-
-          <!-- Categories Widget -->
-          <div class="card my-4">
-            <h5 class="card-header">Categories</h5>
-            <div class="card-body">
-              <div class="row">
-                <div class="col-lg-6">
-                  <ul class="list-unstyled mb-0">
-                    <li>
-                      <a href="#">Web Design</a>
-                    </li>
-                    <li>
-                      <a href="#">HTML</a>
-                    </li>
-                    <li>
-                      <a href="#">Freebies</a>
-                    </li>
-                  </ul>
-                </div>
-                <div class="col-lg-6">
-                  <ul class="list-unstyled mb-0">
-                    <li>
-                      <a href="#">JavaScript</a>
-                    </li>
-                    <li>
-                      <a href="#">CSS</a>
-                    </li>
-                    <li>
-                      <a href="#">Tutorials</a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <!-- Start Side Widget -->
-          <?php include 'widget.php'  ?>
-          <!--  END Side Widget -->
-
-        </div>
+        <!-- Start Side Widget -->
+        <?php include 'widget.php'  ?>
+        <!--  END Side Widget -->
 
       </div>
-      <!-- /.row -->
 
     </div>
+    <!-- /.row -->
+
+  </div>
+  <!-- /.container -->
+
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Your Website 2018</p>
+    </div>
     <!-- /.container -->
+  </footer>
 
-    <!-- Footer -->
-    <footer class="py-5 bg-dark">
-      <div class="container">
-        <p class="m-0 text-center text-white">Copyright &copy; Your Website 2018</p>
-      </div>
-      <!-- /.container -->
-    </footer>
-
-    <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 </body>
 
