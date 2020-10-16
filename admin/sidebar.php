23245f72a78fb2cdc012b09d16060ff00a5d3545
hamed-zeidabadi <zeidabadi73@gmail.com>
2020-10-16 21:08:08 +0330

    add sidebar and header components

---
+++ admin/sidebar.php
@@ -0,0 +1,28 @@
+  <nav>
+      <ul class="sidebar-menu" data-widget="tree">
+
+
+
+          <li class="active"><a href="index.php"><i class="zmdi zmdi-view-dashboard"></i><span>داشبورد</span></a></li>
+
+
+
+          <!-- <li class="treeview">
+              <a href="javascript:void(0)"><i class="zmdi zmdi-apps"></i> <span>برنامه ها</span> <i class="fa fa-angle-left"></i></a>
+              <ul class="treeview-menu">
+                  <li><a href="calendar.html">تقویم</a></li>
+                  <li><a href="chat-box.html">گپ</a></li>
+                  <li><a href="project-list.html">لیست پروژه</a></li>
+                  <li><a href="project-details.html">جزئیات پروژه</a></li>
+              </ul>
+          </li>
+          <li class="treeview">
+              <a href="javascript:void(0)"><i class="zmdi zmdi-email"></i> <span>پست الکترونیک</span> <i class="fa fa-angle-left"></i></a>
+              <ul class="treeview-menu">
+                  <li><a href="mail-inbox.html">صندوق ورودی</a></li>
+                  <li><a href="mail-view.html">مشاهده ایمیل</a></li>
+                  <li><a href="compose-mail.html">ارسال نامه ایمیل</a></li>
+              </ul>
+          </li> -->
+
+  </nav>
